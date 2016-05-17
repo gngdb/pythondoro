366ddcd7cbe07e3f0628369cba7c96f66b285917
Gavin <gavingray1729@gmail.com>
2016-05-17 10:32:31 +0100

    uses tqdm messages properly and doesn't need to flush stdout

--- start.py
+++ start.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 
 import time
-import sys
-from tqdm import tqdm
+from tqdm import trange
 
 def main():
-    print("Working time...")
-    sys.stdout.flush()
-    for t in tqdm(range(60*25)):
+    bar = trange(60*25)
+    bar.write("Working time...")
+    for t in bar:
         time.sleep(1)
-    print("Break time...")
-    sys.stdout.flush()
-    for t in tqdm(range(60*5)):
+    bar = trange(60*5)
+    bar.write("Break time...")
+    for t in bar:
         time.sleep(1)
 
 if __name__ == "__main__":
